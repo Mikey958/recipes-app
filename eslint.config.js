8c427ca8f32e2f6f0c96a1f070607d1f577bfb4b
Andrei Uzlov <astanovykh@gmail.com>
2026-04-05 22:29:28 +0500

    Добавлена настройка ESLint и Prettier

---
+++ eslint.config.js
@@ -0,0 +1,79 @@
+// @ts-check
+const path = require('node:path');
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  {
+    ignores: ['dist/**', '.angular/**', 'coverage/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      angular.configs.tsRecommended,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: path.resolve(__dirname),
+      },
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      curly: ['error', 'all'],
+      eqeqeq: ['error', 'always'],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      angular.configs.templateRecommended,
+      angular.configs.templateAccessibility,
+      eslintConfigPrettier,
+    ],
+    rules: {
+      '@angular-eslint/template/button-has-type': 'error',
+    },
+  },
+]);
